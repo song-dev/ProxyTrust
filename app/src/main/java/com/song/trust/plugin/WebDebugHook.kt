3ab90db5720654ef87370db6315dd7f9b19cad46
chensongsong <chensongsong@geetest.com>
2021-11-16 16:31:00 +0800

    [feature/1.0.0][update][ WebDebugHook 调试成功 ]

--- app/src/main/java/com/song/trust/plugin/WebDebugHook.kt
+++ app/src/main/java/com/song/trust/plugin/WebDebugHook.kt
@@ -1,8 +1,10 @@
 package com.song.trust.plugin
 
 import android.webkit.WebView
+import com.song.trust.utils.XposedLogger
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 
 /**
  * Created by chensongsong on 2021/11/12.
@@ -10,6 +12,7 @@ import de.robv.android.xposed.XposedBridge
 class WebDebugHook {
 
     fun hook() {
+        XposedLogger.log("WebDebugHook.hook")
         XposedBridge.hookAllMethods(
             WebView::class.java,
             "setWebContentsDebuggingEnabled",
@@ -17,9 +20,39 @@ class WebDebugHook {
                 override fun beforeHookedMethod(param: MethodHookParam?) {
                     super.beforeHookedMethod(param)
                     param?.args?.set(0, true)
+                    XposedLogger.log("WebView.setWebContentsDebuggingEnabled Parameter: true")
                 }
             }
         )
+
+        XposedBridge.hookAllMethods(
+            WebView::class.java,
+            "getSettings",
+            object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam?) {
+                    super.afterHookedMethod(param)
+                    // 执行静态 setWebContentsDebuggingEnabled 方法
+                    XposedLogger.log("WebView Constructor callStaticMethod setWebContentsDebuggingEnabled(true)")
+                    XposedHelpers.callStaticMethod(
+                        WebView::class.java,
+                        "setWebContentsDebuggingEnabled",
+                        true
+                    )
+                }
+            })
+
+//        XposedBridge.hookAllConstructors(WebView::class.java, object : XC_MethodHook() {
+//            override fun afterHookedMethod(param: MethodHookParam?) {
+//                super.afterHookedMethod(param)
+//                // 执行静态 setWebContentsDebuggingEnabled 方法
+//                XposedLogger.log("WebView Constructor callStaticMethod setWebContentsDebuggingEnabled(true)")
+//                XposedHelpers.callStaticMethod(
+//                    WebView::class.java,
+//                    "setWebContentsDebuggingEnabled",
+//                    true
+//                )
+//            }
+//        })
 
     }
 
